e6d11bd37b6fc2041575d5a9c790a3d2a1aa34e6
Sparsh261 <sparshchauhan112@gmail.com>
2025-06-09 12:08:56 +0530

    Change Notifier

--- lib/FirstScreen.dart
+++ lib/FirstScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management/ListProvider.dart';
 import 'package:state_management/SecondScreen.dart';
 
 class FirstScreen extends StatefulWidget {
@@ -8,34 +10,34 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
-  List<int> number = [1];
+  
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Consumer(
+      builder:(BuildContext context, NumberListProvider nlp, Widget? child)=>
+    Scaffold(
       appBar: AppBar(title: Text("First Screen"),),
       floatingActionButton: FloatingActionButton(onPressed: (){
-        setState(() {
-          number.add(number.last+1);
-        });
+        nlp.add();
       },
         child: Icon(Icons.add),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(number[number.length-1].toString(), style: TextStyle(fontSize: 30),),
+          Text(nlp.number[nlp.number.length-1].toString(), style: TextStyle(fontSize: 30),),
           Expanded(
             child: ListView.builder(
-              itemCount: number.length,
-              itemBuilder: (context,index)=>Text(number[index].toString())
+              itemCount: nlp.number.length ,
+              itemBuilder: (context,index)=>Text(nlp.number[index].toString())
             ),
           ),
           ElevatedButton(onPressed: (){
-            Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=>SecondScreen(number: number)));
+            Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=>SecondScreen()));
           }, child: Text("Navigate"))
         ],
       ),
-    );
+    ));
   }
 }
--- lib/SecondScreen.dart
+++ lib/SecondScreen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management/ListProvider.dart';
 
 class SecondScreen extends StatefulWidget {
-  final List<int> number;
-  SecondScreen({required this.number});
   @override
   State<SecondScreen> createState() => _SecondScreenState();
 }
@@ -10,36 +10,36 @@ class SecondScreen extends StatefulWidget {
 class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Consumer(
+      builder: (BuildContext context, NumberListProvider nlp, Widget? child)=>
+    Scaffold(
       appBar: AppBar(title: Text('SECOND SCREEN')),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           //last +1
           //add to number
-          setState(() {
-            widget.number.add(widget.number.last + 1);
-          });
+          nlp.add();
         },
         child: Icon(Icons.add),
       ),
       body: Column(
         children: [
           Text(
-            widget.number.last.toString(),
+            nlp.number.last.toString(),
             style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: widget.number.length,
+              itemCount: nlp.number.length,
               itemBuilder:
                   (context, index) => Text(
-                    widget.number[index].toString(),
+                    nlp.number[index].toString(),
                     style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   ),
             ),
           ),
         ],
       ),
-    );
+    ));
   }
 }
